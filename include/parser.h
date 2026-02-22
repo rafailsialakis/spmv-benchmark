ddfa401648bac5eced6d375f04aad2360ec65789
rafailsialakis <rafailsiala@gmail.com>
2026-02-22 21:49:03 +0200

    Finished the structure of the parser. Added comments to all function declarations, removed code from parser.h

--- include/parser.h
+++ include/parser.h
@@ -1,5 +1,31 @@
+/*
+ * This header files contains the structs and functions needed to parse a *.mtx file and generate its Coordinate Format.
+ * The format of a *.mtx file is:
+ * %%MatrixMarket <object> <format> <field> <symmetry>
+ * % Comment lines (optional)
+ * <nrows> <ncols> <nnz>
+ * <i_1> <j_1> <value_1>
+ * <i_2> <j_2> <value_2>
+ * ...
+ * <i_n> <j_n> <value_n>
+ */
+
 #ifndef PARSER_H   
 #define PARSER_H
+
+/*
+ * Represents the metadata information of a SuiteSparse .mtx file.
+ *
+ * Fields:
+ *     rows (int): The number of rows the matrix has.
+ *     cols (int): The number of columns the matrix has.
+ *     nnz (int): The number of non-zero elements.
+ */
+struct Metadata {
+    int    rows;
+    int    cols;
+    int    nnz;
+};
 
 /*
  * Represents the header information of a SuiteSparse .mtx file.
@@ -12,23 +38,74 @@
  *     symmetry (char[16]): Symmetry property of the matrix ("general", "symmetric", etc.).
  */
 struct MtxType{
-    char name[16];
-    char object[16];
-    char format[16];
-    char field[16];
-    char symmetry[16];
+    char   name[16];
+    char   object[16];
+    char   format[16];
+    char   field[16];
+    char   symmetry[16];
 };
 
-/* Parses the header line of a SuiteSparse .mtx file.
+/*
+ * Represents the Coordinate Format of a Sparse Matrix.
+ *
+ * Fields:
+ *     metadata (struct Metadata): Struct that contains information about the number of rows, cols and nnz.
+ *     row_idx (int*):
+ *     col_idx (int*):
+ *     values (double*):
+ */
+struct COOMatrix {
+    struct Metadata metadata;
+    int    *row_idx;
+    int    *col_idx;
+    double *values;
+};
+
+/* This function parses the header line of a SuiteSparse .mtx file.
  * 
  * Args:
  *     mtxType (struct MtxType*): Pointer to a struct where the header information will be stored.
  *     file (FILE*): Pointer to an already open .mtx file.
  */
-void parse_type(struct MtxType* mtxType, FILE* file){
-    fscanf(file, "%*s %15s %15s %15s %15s",
-           mtxType->object, mtxType->format, mtxType->field, mtxType->symmetry);
-    return;
-}
+void parse_header(struct MtxType* mtx_type, FILE* file);
+
+/* This function skips all the comment lines (starting with %) except the one with the name of the matrix.
+ * Also it parses its metadata, which is in the line after the last comment. 
+ * 
+ * Args:
+ *     mtx_type (struct MtxType*): Pointer to a struct where the header information will be stored.
+ *     coo_mtx (struct COOMatrix*): Pointer to a struct that will create the COOMatrix format.
+ *     file (FILE*): Pointer to an already open .mtx file.
+ */
+void parse_metadata(struct MtxType* mtx_type, struct COOMatrix* coo_mtx, FILE* file);
+
+/* This function reads all the noncomment lines of the *.mtx file and generates the Coordinate Format of the matrix.
+ * 
+ * Args:
+ *     mtx_type (struct MtxType*): Pointer to a struct where the header information will be stored.
+ *     coo_mtx (struct COOMatrix*): Pointer to a struct that will create the COOMatrix format.
+ *     file (FILE*): Pointer to an already open .mtx file.
+ * 
+ * Notes:
+ *     - The function assumes that the function parse_metadata() has ran so that the struct Metadata metadata field has been initialized.
+ *       If it is not initialized, memory cannot be allocated.
+ *     - The FILE pointer points right at the beginning of the entries which share the format: row col value
+ */
+void parse_coo(struct COOMatrix* coo_mtx, FILE* file);
+
+/* This function opens a files and exits if the file name does not exist.
+ * 
+ * Args:
+ *     filename (char*): A String that represents the name of the file that will be opened.
+ */
+FILE* open_file(char* filename);
+
+/* This function is used to concatinate the path into the matrices folder.
+ * More presicely it concatinates the String "../matrices" (which represents the path to the folder where *.mtx files are saved) with the name of the .mtx file.
+ * 
+ * Args:
+ *     filename (char*): A String that represents the name of the file that will be opened.
+ */
+char* reconstruct_path(char* filename);
 
 #endif
--- src/parser.c
+++ src/parser.c
@@ -2,29 +2,84 @@
 #include <stdlib.h>
 #include <string.h>
 #include "../include/parser.h"
-#define EXIT_SUCCESS 0 
 
-int main(int argc, char* argv[]){
-    char* filename = argv[1];
-    if(argc != 2){
-        printf("Error: Invalid number of arguments.\nUsage: %s <file.mtx>\n",argv[0]);
-        return(EXIT_FAILURE);
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("Error: Invalid number of arguments.\nUsage: %s <file.mtx>\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
+    char* result = reconstruct_path(argv[1]);
+    FILE* mtx = open_file(result);
+    free(result);
+
+    struct MtxType mtx_type = {0};
+    struct COOMatrix coo_mtx = {0};
+
+    parse_header(&mtx_type, mtx);
+    parse_metadata(&mtx_type, &coo_mtx, mtx);
+    parse_coo(&coo_mtx, mtx);
+
+    fclose(mtx);
+    free(coo_mtx.row_idx);
+    free(coo_mtx.col_idx);
+    free(coo_mtx.values);
+
+    return EXIT_SUCCESS;
+}
+
+void parse_header(struct MtxType* mtx_type, FILE* file){
+    fscanf(file, "%*s %15s %15s %15s %15s\n",
+           mtx_type->object, mtx_type->format, mtx_type->field, mtx_type->symmetry);
+    return;
+}
+
+void parse_metadata(struct MtxType* mtx_type, struct COOMatrix* coo_mtx, FILE* file){
+    char line[256];
+    while (fgets(line, sizeof(line), file) != NULL) {
+        // Skip comment lines except the line that saves the name.
+        if (line[0] == '%'){
+            sscanf(line, "%% name: %255s", mtx_type->name);            
+            continue;
+        }
+        // Parse metadata line.
+        sscanf(line, "%ld %ld %ld", &coo_mtx->metadata.rows, &coo_mtx->metadata.cols, &coo_mtx->metadata.nnz);
+        break;
+    }
+}
+
+void parse_coo(struct COOMatrix* coo_mtx, FILE* file) {
+    coo_mtx->row_idx = malloc(sizeof(int)    * coo_mtx->metadata.nnz);
+    coo_mtx->col_idx = malloc(sizeof(int)    * coo_mtx->metadata.nnz);
+    coo_mtx->values  = malloc(sizeof(double) * coo_mtx->metadata.nnz);
+
+    char line[256];
+    for (int i = 0; i < coo_mtx->metadata.nnz; i++) {
+        fgets(line, sizeof(line), file);
+        sscanf(line, "%d %d %lf", &coo_mtx->row_idx[i],
+                                   &coo_mtx->col_idx[i],
+                                   &coo_mtx->values[i]);
+        coo_mtx->row_idx[i]--;
+        coo_mtx->col_idx[i]--;
+        printf("%d %d %e\n", coo_mtx->row_idx[i],
+                                   coo_mtx->col_idx[i],
+                                   coo_mtx->values[i]);
+    }
+}
+
+FILE* open_file(char* filename){
+    FILE* f = fopen(filename, "r");
+    if(f == NULL){
+        printf("Error: Failed to open file %s: No such file or directory\n",filename);
+        exit(EXIT_FAILURE);
+    }
+    return f;
+}
+
+char* reconstruct_path(char* filename){
     char* path = "../matrices/";
     char* result = (char*) malloc(strlen(filename) + strlen(path) + 1);
     strcpy(result, path);
     strcat(result, filename);
-    
-    FILE* mtx = fopen(result, "r");
-    if(mtx == NULL){
-        printf("Error: Failed to open file %s: No such file or directory\n",filename);
-        return(EXIT_FAILURE);
-    }
-    char buf[64];
-    char* line;
-    struct MtxType mtxType;
-    parse_type(&mtxType, mtx);
-
-    printf("%s\n", mtxType.object);        
+    return result;
 }
