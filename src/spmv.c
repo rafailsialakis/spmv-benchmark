e24b38c8feabfc41a2e8374a80aa270649deda6f
rafailsialakis <rafailsiala@gmail.com>
2026-03-02 20:35:26 +0200

    Added comments

--- src/spmv.c
+++ src/spmv.c
@@ -4,6 +4,7 @@
 #include "../include/spmv.h"
 
 void spmv_csr(struct CSRMatrix* csr, double* x, double* y) {
+    // Parallel for over rows
     #pragma omp parallel for schedule(static)
     for (int i = 0; i < csr->n; i++) {
         double sum = 0.0;
