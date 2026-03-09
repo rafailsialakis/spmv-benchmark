52c7c43f4b88f6742f1174e30b1657155f53c1ef
rafailsialakis <rafailsiala@gmail.com>
2026-03-09 23:41:50 +0200

    added benchmark ios

--- src/benchmark.c
+++ src/benchmark.c
@@ -2,19 +2,43 @@
 #include <stdlib.h>
 #include <float.h>
 #include "../include/csr.h"
-#define N_RUNS 100
+#include "../include/spmv.h"
+#include "../include/timer.h"
+#include "../include/benchmark.h"
 
 double compute_gflops(int nnz, double time_sec) {
     return (2.0 * nnz) / (time_sec * 1e9);
 }
 
-void run_benchmark(const char* label, struct CSRMatrix* csr, double* x, double* y) {
+void run_benchmark_rax(const char* label, struct CSRMatrix* csr, double* x, double* y) {
     double best_time = DBL_MAX;
     for (int i = 0; i < N_RUNS; i++) {
         double start = get_time();
         spmv_csr(csr, x, y);
         double end = get_time();
         double t = end - start;
+        if (t < best_time) best_time = t;
+    }
+
+    double gflops = compute_gflops(csr->nnz, best_time);
+    printf("%-20s time: %.3f ms   GFLOP/s: %.2f\n",
+           label, best_time * 1000.0, gflops);
+}
+
+void run_benchmark_ios(const char* label, struct CSRMatrix* csr, double* x, double* y) {
+    double best_time = DBL_MAX;
+    for (int i = 0; i < N_RUNS; i++) {
+        double start, end, t;
+        if (i % 2 == 0) {
+            start = get_time();
+            spmv_csr(csr, x, y);
+            end = get_time();
+        } else {
+            start = get_time();
+            spmv_csr(csr, y, x);
+            end = get_time();
+        }
+        t = end - start;
         if (t < best_time) best_time = t;
     }
 
