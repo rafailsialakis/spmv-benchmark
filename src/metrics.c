5f6b0f1ffc7fd1bf99768fbf1b0a4d78be932345
rafailsialakis <rafailsiala@gmail.com>
2026-03-15 09:19:38 +0200

    Included avg bw instead of just max bw as a metric

--- src/metrics.c
+++ src/metrics.c
@@ -3,18 +3,23 @@
 #include "../include/csr.h"
 #include "../include/metrics.h"
 
-int compute_bandwidth(struct CSRMatrix* csr){
-    int max_bandwidth = -1;
+struct BWResult compute_bandwidth(struct CSRMatrix* csr){
+    struct BWResult bandwidth_result;
+    bandwidth_result.max_bw = -1;
+    bandwidth_result.avg_bw = 0;
+    long long bw_sum = 0;
     for(int i = 0; i < csr->n; i++){
         for(int j = csr->row_ptr[i]; j < csr->row_ptr[i+1]; j++){
             int col = csr->col_idx[j];
             int bw = abs(i - col);
-            if(bw > max_bandwidth){
-                max_bandwidth = bw;
+            if(bw > bandwidth_result.max_bw){
+                bandwidth_result.max_bw = bw;
             }
+            bw_sum += bw;
         }
     }
-    return max_bandwidth;
+    bandwidth_result.avg_bw = (double)bw_sum / csr->nnz;
+    return bandwidth_result;
 }
 
 double compute_density(struct CSRMatrix* csr){
@@ -26,13 +31,10 @@ double compute_imbalance_ratio(struct CSRMatrix* csr, int threads){
     int rows_per_thread = csr->n / threads;
     for(int t = 0; t < threads; t++){
         int row_start = t * rows_per_thread;
-        int row_end = (t + 1) * rows_per_thread;
-        if (t == (threads - 1))
-            row_end = csr->n;
+        int row_end = (t == threads-1) ? csr->n : (t+1) * rows_per_thread;
         int nnz_thread = csr->row_ptr[row_end] - csr->row_ptr[row_start];
-        if(nnz_thread > max_nnz_thread){
+        if(nnz_thread > max_nnz_thread)
             max_nnz_thread = nnz_thread;
-        }
     }
     return (double)max_nnz_thread / ((double)csr->nnz / threads);
 }
